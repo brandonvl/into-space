98da3716fae128efadb66c206753a150c64e0e18
brandonvl <6137891+brandonvl@users.noreply.github.com>
2020-08-14 20:40:00 +0200

    Add comment to make it clearer why certain functions are not scoped

--- IntoSpace/main.cpp
+++ IntoSpace/main.cpp
@@ -5,7 +5,7 @@
 
 #include <iostream>
 
-// GLFW function declerations
+// GLFW function declerations, need to be global
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
 
